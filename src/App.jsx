4931638625d4cf8ac67633ff850b70321f79adb3
anshshah23 <anshzeal2328@gmail.com>
2024-06-28 19:44:22 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React, { Suspense, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import CustomLoader3 from './components/Loaders/loader3/CustomLoader3';
 import Navbar from './components/Navbar';
-import './App.css';
-
-// Lazy-loaded components
 const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
 const Projects = React.lazy(() => import('./pages/Projects'));
 const ContactForm = React.lazy(() => import('./pages/ContactForm'));
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
-    // Simulating loading time with setTimeout
+    setLoading(true);
+
     const timeout = setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 500);
 
     return () => clearTimeout(timeout);
-  }, []);
+  }, [location]);
 
   return (
     <div className="App text-white bg-gradient-to-br bg-fixed from-green-400 to-blue-950 h-screen">
@@ -29,19 +29,39 @@ function App() {
           <CustomLoader3 />
         </div>
       ) : (
-        <>
+        <div className="App text-white bg-gradient-to-br bg-fixed from-green-400 to-blue-950 h-screen">
           <Navbar />
           <div className="pt-24 p-4">
-            <Suspense fallback={ <div className='text-transparent'>Loading..</div>}>
+            <Suspense fallback={<div className='flex cursor-none justify-center items-center'><CustomLoader3 color="#fff" loading={true} size={150} /></div>}>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<ContactForm />} />
+                <Route
+                  path="/"
+                  element={
+                      <Home />
+                  }
+                />
+                <Route
+                  path="/about"
+                  element={
+                      <About />
+                  }
+                />
+                <Route
+                  path="/projects"
+                  element={
+                      <Projects />
+                  }
+                />
+                <Route
+                  path="/contact"
+                  element={
+                      <ContactForm />
+                  }
+                />
               </Routes>
             </Suspense>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
