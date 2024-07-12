9b0695b5fb2811fd6f91b4470032578db7484d26
anshshah23 <anshzeal2328@gmail.com>
2024-07-12 16:51:39 +0530

    Styling the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { CgPhone } from 'react-icons/cg';
 import '../components/styles/Home.css';
 const Home = () => {
   return (
-    <div className="flex flex-col lg:pt-20 lg:pl-52 lg:mt-2 lg:flex-row items-center justify-center bg-transparent text-white">
+    <div className="flex flex-col lg:pt-20 mb-20 lg:pl-52 lg:mt-2 lg:flex-row items-center justify-center bg-transparent text-white">
       <div className="p-10 lg:w-1/2">
         <h1 className="text-2xl lg:text-5xl">Hello there!</h1>
         <h1 className='text-2xl lg:text-5xl'>I'm </h1>
