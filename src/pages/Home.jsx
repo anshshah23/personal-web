cd18bbe1d72d09b7e837f0b86519869d94439680
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-07-15 17:39:47 +0530

    Minor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,41 +7,44 @@ import { CgPhone } from 'react-icons/cg';
 import '../components/styles/Home.css';
 const Home = () => {
   return (
-    <div className="flex flex-col mb-20 lg:pl-52 lg:mt-2 lg:flex-row items-center justify-center bg-transparent text-white overflow-hidden">
-      <div className="p-10 lg:w-1/2">
-        <h1 className='text-2xl lg:text-5xl'>I'm </h1>
-        <h1 className="text-4xl lg:text-8xl font-bold">
-          <span className="text-purple-400">Ansh Shah</span>
-        </h1>
-        <Type />
-        <div className="mt-8 flex flex-row pr-4">
-          <Link to="/about">
-            <button
-              className="items-center mr-4 justify-center home-button mt-8"
-            >
-              About-Me
-              <BsPerson className="inline-block md:ml-2" />
-            </button>
-          </Link>
-          <Link to="/contact">
-            <button
-              className="items-center justify-center home-button mt-8"
-            >
-              Contact
-              <CgPhone className="inline-block sm:ml-2" />
-            </button>
-          </Link>
+    <div>
+      <h1 className="text-xl text-center">Visit my main website: <a href="https://anshshah.vercel.app" className='text-blue-700 hover:underline'>Click Here</a></h1>
+      <div className="flex flex-col mb-20 lg:pl-52 lg:mt-2 lg:flex-row items-center justify-center bg-transparent text-white overflow-hidden">
+        <div className="p-10 lg:w-1/2">
+          <h1 className='text-2xl lg:text-5xl'>I'm </h1>
+          <h1 className="text-4xl lg:text-8xl font-bold">
+            <span className="text-purple-400">Ansh Shah</span>
+          </h1>
+          <Type />
+          <div className="mt-8 flex flex-row pr-4">
+            <Link to="/about">
+              <button
+                className="items-center mr-4 justify-center home-button mt-8"
+              >
+                About-Me
+                <BsPerson className="inline-block md:ml-2" />
+              </button>
+            </Link>
+            <Link to="/contact">
+              <button
+                className="items-center justify-center home-button mt-8"
+              >
+                Contact
+                <CgPhone className="inline-block sm:ml-2" />
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="lg:w-1/2 mt-8 lg:mt-0">
-        <div>
-          <Link to="/about">
-            <img
-              className="w-64 h-auto lg:w-96 lg:h-auto transform transition-transform duration-500 hover:scale-110"
-              src={Profile}
-              alt="Profile"
-            />
-          </Link>
+        <div className="lg:w-1/2 mt-8 lg:mt-0">
+          <div>
+            <Link to="/about">
+              <img
+                className="w-64 h-auto lg:w-96 lg:h-auto transform transition-transform duration-500 hover:scale-110"
+                src={Profile}
+                alt="Profile"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
